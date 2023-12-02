6161cd0848a43a62cf39e1fe96025c9eb46b220d
i.karpov <karpovia@volgir.com>
2023-12-02 18:18:31 +0400

    optimization

--- src/com/volgir/homework/second/office/Office.java
+++ src/com/volgir/homework/second/office/Office.java
@@ -1,8 +1,19 @@
 package com.volgir.homework.second.office;
 
 public class Office {
+    Chief chief;
+    Manager manager;
+    Secretary secretary;
+    Security security;
 
-    public void workingDay(Chief chief, Manager manager, Secretary secretary, Security security) {
+    public Office(Chief chief, Manager manager, Secretary secretary, Security security) {
+        this.chief = chief;
+        this.manager = manager;
+        this.secretary = secretary;
+        this.security = security;
+    }
+
+    public void workingDay() {
         chief.pushTheManager(manager.name);
         manager.scream();
         security.advanceRequest();
--- src/com/volgir/homework/second/office/Runner.java
+++ src/com/volgir/homework/second/office/Runner.java
@@ -6,8 +6,9 @@ public class Runner {
         Manager manager = new Manager("Володя");
         Secretary secretary = new Secretary();
         Security security = new Security("Петрович");
-        Office office = new Office();
 
-        office.workingDay(chief, manager, secretary, security);
+        Office office = new Office(chief, manager, secretary, security);
+
+        office.workingDay();
     }
 }
