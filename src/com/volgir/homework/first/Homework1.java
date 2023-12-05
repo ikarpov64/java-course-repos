3b7da3692444f5333d90d58b181cf99498ec7c0b
i.karpov <tri3xxx@yandex.ru>
2023-12-05 12:04:19 +0400

    update hw1

--- src/com/volgir/homework/first/Homework1.java
+++ src/com/volgir/homework/first/Homework1.java
@@ -8,8 +8,7 @@ public class Homework1 {
 //        ex1();
 //        ex2();
 //        ex3();
-//        ex4();
-        ex5();
+        ex4();
     }
 
     public static void ex1() {
@@ -126,71 +125,7 @@ public class Homework1 {
 
         long profitBeforeTaxes = income - outcome;
 
-        int sausageSoldCount = 2000;
-        int hamSoldCount = 8511;
-        int neckSoldCount = 6988;
-
-        int sausageSellingPrice = 800;
-        int sausageCostPrice1 = 412;  // Себестоимость при производстве до 1000 кг.
-        int sausageCostPrice2 = 408;  // Себестоимость при производстве от 1000 до 2000 кг.
-        int sausageCostPrice3 = 404;  // Себестоимость при производстве от 2000 кг.
-        int sausageFinalCost;
-
-        int hamSellingPrice = 350;
-        int hamCostPrice = 275;  // Себестоимость при любом производстве.
-        int hamFinalCost;
-
-        int neckSellingPrice = 500;
-        int neckCostPrice1 = 311;  // Себестоимость при производстве до 500 кг.
-        int neckCostPrice2 = 299;  // Себестоимость при производстве от 500 кг.
-        int neckFinalCost;
-
-        double taxRateBeforeMillion = 0.8;
-        double taxRateBeforeTwoMillion = 0.10;
-        double taxRateAfterTwoMillion = 0.13;
-
-//        int profitBeforeTaxes;
-        double profitAfterTaxes;
-
-
-        // Себестоимость колбасы:
-        if (sausageSoldCount >= 2000) {
-            sausageFinalCost = sausageSoldCount * sausageCostPrice3;
-        } else if (sausageSoldCount >= 1000) {
-            sausageFinalCost = sausageSoldCount * sausageCostPrice2;
-        } else {
-            sausageFinalCost = sausageSoldCount * sausageCostPrice1;
-        }
-
-        // Себестоимость ветчины (sold * costPrice)
-        hamFinalCost = hamSoldCount * hamCostPrice;
-
-        // Себестоимость шейки:
-        if (neckSoldCount < 500) {
-            neckFinalCost = neckSoldCount * neckCostPrice1;
-        } else {
-            neckFinalCost = neckSoldCount * neckCostPrice2 ;
-        }
-
-        int costs = sausageFinalCost + hamFinalCost + neckFinalCost + 1_000_000; // Расходы компании.
-
-        profitBeforeTaxes = income - costs;  // Прибыль до налогов.
-
-        double taxes;
-        if (profitBeforeTaxes > 2_000_000) {
-            taxes = 1_000_000 * taxRateBeforeMillion +
-                    1_000_000 * taxRateBeforeTwoMillion +
-                    taxRateAfterTwoMillion * (profitBeforeTaxes - 2_000_000);
-        } else if (profitBeforeTaxes > 1_000_000) {
-            taxes = 1_000_000 * taxRateBeforeMillion +
-                    taxRateBeforeTwoMillion * (profitBeforeTaxes - 1_000_000);
-        } else if (profitBeforeTaxes > 0) {
-            taxes = profitBeforeTaxes * taxRateBeforeMillion;
-        } else {
-            taxes = 0;
-        }
-
-        profitAfterTaxes = profitBeforeTaxes - taxes;  // Прибыль после налогов.
+        double profitAfterTaxes = calcTax(profitBeforeTaxes); // Прибыль после налогов.
         System.out.printf("Прибыль после налогов: %s руб.%n", profitAfterTaxes);
 
     }
@@ -214,38 +149,23 @@ public class Homework1 {
         double totalTax;
         double firstLimit = TAX_RATE_BEFORE_MILLION.getLimit();
         double secondLimit = TAX_RATE_BEFORE_TWO_MILLION.getLimit();
-//        long third = TAX_RATE_AFTER_TWO_MILLION.getLimit();
         double taxRateBeforeMillion = TAX_RATE_BEFORE_MILLION.getTaxRate();
         double taxRateBeforeTwoMillion = TAX_RATE_BEFORE_TWO_MILLION.getTaxRate();
+        double taxRateAfterTwoMillion = TAX_RATE_AFTER_TWO_MILLION.getTaxRate();
 
+        double totalTaxBeforeMillion = firstLimit * taxRateBeforeMillion;
+        double totalTaxBeforeTwoMillion = (secondLimit - firstLimit - 1) * taxRateBeforeTwoMillion;
 
         if (profitBeforeTaxes > secondLimit) {
-            double totalTaxBeforeMillion = firstLimit * taxRateBeforeMillion;
-            double totalTaxBeforeTwoMillion = (secondLimit - firstLimit - 1) * taxRateBeforeTwoMillion;
-
-            totalTax = profitBeforeTaxes;
-
-            totalTax = TAX_RATE_AFTER_TWO_MILLION.getTaxRate() * (profitBeforeTaxes - TAX_RATE_AFTER_TWO_MILLION.getLimit()) + totalTaxBeforeMillion + totalTaxBeforeTwoMillion;
-        } else if (profitBeforeTaxes > 1_000_000) {
-            totalTax = 1_000_000 * taxRateBeforeMillion +
-                    taxRateBeforeTwoMillion * (profitBeforeTaxes - 1_000_000);
+            totalTax = (profitBeforeTaxes - secondLimit) * taxRateAfterTwoMillion
+                    + totalTaxBeforeTwoMillion + totalTaxBeforeMillion;
+        } else if (profitBeforeTaxes > firstLimit) {
+            totalTax = (profitBeforeTaxes - firstLimit) * taxRateBeforeTwoMillion + totalTaxBeforeMillion;
         } else if (profitBeforeTaxes > 0) {
             totalTax = profitBeforeTaxes * taxRateBeforeMillion;
         } else {
             totalTax = 0;
         }
         return totalTax;
-    }
-
-    public static void ex5() {
-        // Получение цен продуктов
-        int sausagePrice = SAUSAGE.getPrice();
-        int hamPrice = HAM.getPrice();
-        int neckPrice = NECK.getPrice();
-
-        // Вывод цен продуктов
-        System.out.println("Price of SAUSAGE: " + sausagePrice);
-        System.out.println("Price of HAM: " + hamPrice);
-        System.out.println("Price of NECK: " + neckPrice);
     }
 }
